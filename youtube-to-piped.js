9c940de1491c9e1d62293b8ee4f729da228ece56
ghost <116072981+greysonGhost@users.noreply.github.com>
2023-05-03 20:11:19 +0800

    Update youtube-to-piped.js

--- youtube-to-piped.js
+++ youtube-to-piped.js
@@ -1,1 +1,3 @@
-javascript:(function(){window.location=window.location.toString().replace(/^https:\/\/www.youtube.com/,'https://piped.kavin.rocks');})()
+javascript: (function() {
+    window.location = window.location.toString().replace(/^https:\/\/www.youtube.com/, 'https://piped.kavin.rocks');
+})()
