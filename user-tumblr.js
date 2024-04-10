2df831d20eb1fcb3734eea4586076d886fbc7b89
milo !! <116072981+greysonGhost@users.noreply.github.com>
2024-04-10 13:42:28 +0800

    Update user-tumblr.js
    condensed 2 lines

--- user-tumblr.js
+++ user-tumblr.js
@@ -1,7 +1,6 @@
 javascript: (function() {
   let regex = /(?<=\.com\/)\w+/gm;
   let url = window.location.toString();
-  let username = url.match(regex);
-  let user = username[0];
-  window.location = ("https://".concat(user, ".tumblr.com"));
+  let username = (url.match(regex))[0];
+  window.location = ("https://".concat(username, ".tumblr.com"));
 })()
